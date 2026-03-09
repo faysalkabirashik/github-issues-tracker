1ac1af2bc0f50cc173fc122f823c3c337cde87c3
Faysal Kabir Ashik <121598938+faysalkabirashik@users.noreply.github.com>
2026-03-09 20:29:32 +0600

    Designed successfully the login page with credential

---
+++ js/login.js
@@ -0,0 +1,13 @@
+function loginUser(){
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if(username === "admin" && password === "admin123"){
+        window.location.href = "main.html";
+    }
+    else{
+        alert("Invalid username or password");
+    }
+
+}
